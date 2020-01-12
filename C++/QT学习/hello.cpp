1e06a63dd7a6b7fbdeafef1c7dc4428ff03dba0e
AustinYANyh <1241528539@qq.com>
2020-01-12 22:23:27 +0800

    对QT中多行文本编辑器的一点点的了解

--- C++/QT学习/hello.cpp
+++ C++/QT学习/hello.cpp
@@ -1,18 +1,49 @@
 #include "hello.h"
-#include<iostream>
+#include <QStringListModel>
+#include <QStandardItemModel>
+#include <QModelIndex>
+#include <QMessageBox>
+#include <iostream>
+#include <windows.h>
 
 using namespace std;
+
+int a = 0;
 
 hello::hello(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 	connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(setText()));
+	connect(ui.BTN_ADD, SIGNAL(clicked()), this, SLOT(addMessage()));
+	connect(ui.BTN_CLEAR, SIGNAL(clicked()), this, SLOT(clearText()));
 }
 
 void hello::setText()
 {
 	QString text = "wo xi huan luzihan!~";
-	ui.EDI->setText(((text)));
-	//ui.EDI->setText(tr(QString::fromLocal8Bit(text)));
+	ui.EDI->setText((QStringLiteral("我最喜欢陆子涵!~")));
+}
+
+void hello::clearText()
+{
+	ui.EDI->clear();
+	ui.EDI->setFocus();
+}
+
+void hello::addMessage()
+{
+	//QMessageBox::information(this, QStringLiteral("嗷哟"), QStringLiteral("入门太复杂了!~不愧是C++!~"));
+	if (a % 2 == 0)
+	{
+		ui.EDI_MESSAGE->setAlignment(Qt::AlignRight);
+		ui.EDI_MESSAGE->append(QStringLiteral("你好!~"));
+		a++;
+	}
+	else
+	{
+		ui.EDI_MESSAGE->setAlignment(Qt::AlignLeft);
+		ui.EDI_MESSAGE->append(QStringLiteral("不好!~"));
+		a++;
+	}
 }
