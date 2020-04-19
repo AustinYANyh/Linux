e6fdb38dd72ca724ce47ed4ed0efe3968bdae44d
AustinYANyh <1241528539@qq.com>
2020-04-19 12:54:40 +0800

    在不复习C++就真的快忘完了,搞C#也不能放掉C++...复习单例模式,指针

---
+++ C++/review/testInstince.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+mutex g_mutex;
+
+class TeseInstance
+{
+public:
+	static TeseInstance* GetInstance()
+	{
+		g_mutex.lock();
+		static TeseInstance* _instance;
+		if (_instance == nullptr)
+			_instance = new TeseInstance();
+		g_mutex.unlock();
+		return _instance;
+
+		
+	}
+
+	int Add(int a, int b)
+	{
+		return a + b;
+	}
+private:
+	TeseInstance()
+	{
+
+	}
+};
+
+int main()
+{
+	cout << TeseInstance::GetInstance()->Add(1, 2) << endl;
+	cout << "Hello World!\n";
+}
